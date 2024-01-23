ec2098dbb2fc62190d3021ede6ae9dc38e81ee6c
JenIsCoding <ev3rythingisborrow3d@gmail.com>
2024-01-23 14:38:06 +0100

    updating gitignore + build dist folder

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export default[
     {
-        id: 1,
+        id: "jan-24-1",
         month: ["01","January"],
         day: "07",
         year: "2024",
@@ -10,7 +10,7 @@ export default[
         topic: "Part 1: create a Fun facts card about React"
     },
     {
-        id: 2,
+        id: "jan-24-2",
         month: ["01","January"],
         day: "08",
         year: "2024",
@@ -20,7 +20,7 @@ export default[
         topic: "Part 2: create a copy of AirBnb experiences page + SoloProject: creation of a Coding Diary (this project!)"
     },
     {
-        id: 3,
+        id: "jan-24-3",
         month: ["01","January"],
         day: "09",
         year: "2024",
@@ -29,7 +29,7 @@ export default[
         titleResource: "Upload React works on GitHub and deploy the webpages through GH Pages → Epic fail ❌"
     },
     {
-        id: 4,
+        id: "jan-24-4",
         month: ["01","January"],
         day: "10",
         year: "2024",
@@ -38,7 +38,7 @@ export default[
         titleResource: "Upload first React work (1-ReactInfoSite) on GitHub and deploy it as a website through GH Pages → Success ✔️"
     },
     {
-        id: 5,
+        id: "jan-24-5",
         month: ["01","January"],
         day: "11",
         year: "2024",
@@ -47,7 +47,7 @@ export default[
         titleResource: "Upload second React work (2-SP-StaticPage) on GitHub and deploy it as a website through GH Pages + Started part 3 of the React Tutorial"
     },
     {
-        id: 6,
+        id: "jan-24-6",
         month: ["01","January"],
         day: "12",
         year: "2024",
@@ -56,7 +56,7 @@ export default[
         titleResource: "Has been diffucult to deploy my third React+Vite project through GH Pages. I've investigated the web in search of alternatives. I have found Netlify.com."
     },
     {
-        id: 7,
+        id: "jan-24-7",
         month: ["01","January"],
         day: "13",
         year: "2024",
@@ -65,7 +65,7 @@ export default[
         titleResource: "Netlify account created; study the deployment through Netlify. Success 😺!!!!!!!!!!!! It was easiest than GH Page process :)"
     },
     {
-        id: 8,
+        id: "jan-24-8",
         month: ["01","January"],
         day: "14",
         year: "2024",
@@ -74,7 +74,7 @@ export default[
         titleResource: "Almost at the end of this 3rd part of this fantastic tutorial."
     },
     {
-        id: 9,
+        id: "jan-24-9",
         month: ["01","January"],
         day: "14",
         year: "2024",
@@ -83,7 +83,7 @@ export default[
         titleResource: "Update Readme.md of 1-ReactInfoSite project with the link of the deployed site."
     },
 	{
-        id: 10,
+        id: "jan-24-10",
         month: ["01","January"],
         day: "15",
         year: "2024",
@@ -92,22 +92,77 @@ export default[
         titleResource: "Update Readme.md of 1-ReactInfoSite project with the link of the deployed site."
     },
 	{
-        id: 11,
+        id: "jan-24-11",
         month: ["01","January"],
-        day: "15",
+        day: "16",
         year: "2024",
-        programmingLanguage: "React",
+        programmingLanguage: "GitHub",
         linkResource: " ",
-        titleResource: "Create ."
+        titleResource: "Updating GitHub repos."
     },
 	{
-        id: 12,
+        id: "jan-24-12",
         month: ["01","January"],
         day: "17",
         year: "2024",
         programmingLanguage: "React",
         linkResource: " ",
         titleResource: "Upload 5-BuildMemeGenerator project on Github."
+    },
+    {
+        id: "jan-24-13",
+        month: ["01","January"],
+        day: "18",
+        year: "2024",
+        programmingLanguage: "React",
+        linkResource: " ",
+        titleResource: "Studied from online recources."
+    },
+    {
+        id: "jan-24-14",
+        month: ["01","January"],
+        day: "19",
+        year: "2024",
+        programmingLanguage: "GitHub",
+        linkResource: " ",
+        titleResource: "Updating GitHub repos."
+    },
+    {
+        id: "jan-24-15",
+        month: ["01","January"],
+        day: "20",
+        year: "2024",
+        programmingLanguage: "React",
+        linkResource: " ",
+        titleResource: "ReactInfosite version upgraded with dark/light mode toggle."
+    },
+    {
+        id: "jan-24-16",
+        month: ["01","January"],
+        day: "21",
+        year: "2024",
+        programmingLanguage: "React",
+        linkResource: "https://youtu.be/bMknfKXIFA8?t=36050",
+        titleResource: "Followed the tutorial on YT on understand and add some functionalities to an existing React app named react-mde (React MarkDown Editor)."
+    },
+    {
+        id: "jan-24-17",
+        month: ["01","January"],
+        day: "22",
+        year: "2024",
+        programmingLanguage: "React",
+        linkResource: " ",
+        titleResource: "Followed the tutorial on Scrimba (not on YouTube) on adding Firebase database functionalities on the MarkDown Notes app (already existing) on React. I didn't know Firebase: seems pretty interesting :)!"
+    },
+    {
+        id: "jan-24-18",
+        month: ["01","January"],
+        day: "23",
+        year: "2024",
+        programmingLanguage: "React",
+        linkResource: "https://youtu.be/bMknfKXIFA8?t=38657",
+        titleResource: "Following Scrimba Tutorial on YT and on Scrimba itself :D and beginning to implement the Tenzenis React game app."
     }
+
 	
 ]
