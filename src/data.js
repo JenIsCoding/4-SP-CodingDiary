fd2fdf117a02b885e4cdf71f0b805689459bb672
JenIsCoding <ev3rythingisborrow3d@gmail.com>
2024-02-02 11:24:13 +0100

    data.js file updated at today February the 2nd

--- src/data.js
+++ src/data.js
@@ -162,7 +162,86 @@ export default[
         programmingLanguage: "React",
         linkResource: "https://youtu.be/bMknfKXIFA8?t=38657",
         titleResource: "Following Scrimba Tutorial on YT and on Scrimba itself :D and beginning to implement the Tenzenis React game app."
-    }
-
-	
+    },
+    {
+        id: "jan-24-19",
+        month: ["01","January"],
+        day: "24",
+        year: "2024",
+        programmingLanguage: "React",
+        linkResource: "https://youtu.be/bMknfKXIFA8?t=38738",
+        titleResource: "After watching the introduction on the Tenzies game project, have understand the requirements about the game functionalities and check the Figma layout, I've started to think how to implement the React code all alone without following the Scrimba tutotorial."
+    },
+    {
+        id: "jan-24-20",
+        month: ["01","January"],
+        day: "25",
+        year: "2024",
+        programmingLanguage: "React",
+        linkResource: "https://youtu.be/bMknfKXIFA8?t=38738",
+        titleResource: "Continuing implementing the Tenzies game project. I focused mainly on setting the style of the game through CSS."
+    },
+    {
+        id: "jan-24-21",
+        month: ["01","January"],
+        day: "26",
+        year: "2024",
+        programmingLanguage: "React",
+        linkResource: "https://youtu.be/bMknfKXIFA8?t=38738",
+        titleResource: "Continuing implementing the Tenzies game project. Setting the main React components for the App."
+    },
+    {
+        id: "jan-24-22",
+        month: ["01","January"],
+        day: "27",
+        year: "2024",
+        programmingLanguage: "React",
+        linkResource: "https://youtu.be/bMknfKXIFA8?t=38738",
+        titleResource: "Continuing implementing the Tenzies game project. From here, I focused more on implementing the game logic and translate it into React language."
+    },
+    {
+        id: "jan-24-23",
+        month: ["01","January"],
+        day: "28",
+        year: "2024",
+        programmingLanguage: "React",
+        linkResource: "https://youtu.be/bMknfKXIFA8?t=38738",
+        titleResource: "Continuing implementing the Tenzies game project; furthermore, I've been studying some other React concepts."
+    },
+    {
+        id: "jan-24-24",
+        month: ["01","January"],
+        day: "29",
+        year: "2024",
+        programmingLanguage: "React",
+        linkResource: "https://youtu.be/bMknfKXIFA8?t=38738",
+        titleResource: "Still continuing implementing the Tenzies game project. There was not much time to devote to the project today :(."
+    },
+    {
+        id: "jan-24-25",
+        month: ["01","January"],
+        day: "30",
+        year: "2024",
+        programmingLanguage: "React",
+        linkResource: "https://youtu.be/bMknfKXIFA8?t=38738",
+        titleResource: "The project is pretty musch completed. The required functions are present :)!! I began to think to some small features that I would like to add to customize the game and adapt it to my code journey :D!"
+    },
+    {
+        id: "jan-24-26",
+        month: ["01","January"],
+        day: "31",
+        year: "2024",
+        programmingLanguage: "React",
+        linkResource: "https://youtu.be/bMknfKXIFA8?t=38738",
+        titleResource: "Still working on the Tenzies game project. I've add a text above the Roll button that changes according to the stage of the game the user is in; simple but pretty cool!"
+    },
+    {
+        id: "feb-24-1",
+        month: ["01","February"],
+        day: "01",
+        year: "2024",
+        programmingLanguage: "React",
+        linkResource: "https://youtu.be/bMknfKXIFA8?t=38738",
+        titleResource: "Still working on the Tenzies game project. I've added a useState counter variable that counts the steps that user has needed to complete the game! I've also added some emoji to spice the game up :D!"
+    },
 ]
